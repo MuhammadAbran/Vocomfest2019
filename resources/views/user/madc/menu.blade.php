042883b780022396d5604688cd5d40787d8a4aff
adeputr4 <adedwiputra.id@gmail.com>
2019-02-03 01:31:12 +0700

    add active menu styling in madc, wdc and admin menu

--- resources/views/user/madc/menu.blade.php
+++ resources/views/user/madc/menu.blade.php
@@ -1,16 +1,16 @@
 
 @section('menu')
 <ul class="list-unstyled components">
-    <li>
+    <li class="{{ URL::route('madc.dashboard') === URL::current() ? 'active' : '' }}">
         <a href="{{route('madc.dashboard')}}"><i class="fas fa-briefcase"></i> Dashboard</a>
     </li>
-    <li>
+    <li class="{{ URL::route('madc.team') === URL::current() ? 'active' : '' }}">
         <a href="{{route('madc.team')}}"><i class="fas fa-users"></i> Team Member</a>
     </li>
-    <li>
-        <a href="{{route('madc.payment')}}"><i class="fas fa-briefcase"></i>  Payment</a>
+    <li class="{{ URL::route('madc.payment') === URL::current() ? 'active' : '' }}">
+        <a  href="{{route('madc.payment')}}"><i class="fas fa-briefcase"></i>  Payment</a>
     </li>
-     <li>
+     <li class="{{ URL::route('madc.submission') === URL::current() ? 'active' : '' }}">
         <a href="{{route('madc.submission')}}"><i class="fas fa-briefcase"></i> Submit</a>
     </li>
 </ul>
