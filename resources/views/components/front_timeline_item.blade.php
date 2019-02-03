c25c8cae377d376f2000b116e3025cafe32e0d82
adeputr4 <adedwiputra.id@gmail.com>
2019-02-03 23:07:02 +0700

    add timeline in frontend

---
+++ resources/views/components/front_timeline_item.blade.php
@@ -0,0 +1,13 @@
+<div class="timeline-item mb-10">
+    <div class="row">
+        <div class="order-xs-2 col-xs-12  order-sm-2 col-sm-12  order-md-1 col-md-3 ">
+            <div class="date">{{$date}}</div> 
+        </div>    
+        <div class="order-xs-1 col-xs-12  order-sm-1 col-sm-12  order-md-2 col-md-2  ">
+            <div class="icon"><i class="fas fa-lg fa-calendar-alt"></i></div>    
+        </div>
+        <div class="order-xs-3 col-xs-12 order-sm-3 col-sm-12  order-md-3 col-md-7  ">
+            <div class="text">{{$text}}</div>
+        </div>
+    </div>   
+</div>
