1a510a39505cc06a0bb056f7c12caeb6b860d489
adeputr4 <adedwiputra.id@gmail.com>
2019-03-01 08:11:52 +0700

    fix timeline and add prize

--- resources/views/frontend/madc_page.blade.php
+++ resources/views/frontend/madc_page.blade.php
@@ -9,6 +9,16 @@
 @section('competition_description')
     Mobile Apps Development Competition (MADC) merupakan kompetisi pengembangan aplikasi berbasis platform Android. MADC Vocomfest 2019 bertemakan “Raise up indonesia’s Small Industries with Marketplace”.
     Dengan tema tersebut diharapkan dapat memberikan dampak pada perekonomian di Indonesia khususnya untuk mengangkat UMKM di Indonesia.Selain itu diharapkan juga para developer di Indonesia mendapatkan ruang untuk memberikan kontribusi nyatanya pada Indonesia.
+    <br/>
+<div >
+        <h3>Hadiah</h3>
+        
+            <p>Juara 1 <br/> Rp 5000.000,00</p>
+            <p>Juara 2 <br/> Rp 3000.000,00</p>
+            <p>Juara 3 <br/> Rp 1500.000,00</p>
+            <p>Best App <br/> Rp 500.000,00</p>
+        
+</div>
 @endsection
 
 @section('rulebook_btn')
--- resources/views/frontend/wdc_page.blade.php
+++ resources/views/frontend/wdc_page.blade.php
@@ -10,6 +10,15 @@
     Web Design Competition merupakan rangkaian acara pertama dari VOCOMFEST 2019, WDC merupakan kompetisi membuat design Website.
 
     WDC ditujukan untuk siswa SMA/SMK/MA sederajat yang sudah bisa maupun masih belajar dalam mengembangkan website. Peserta berkompetisi dalam tim yang beranggotakan maksimal 3 orang. WDC ini dilaksanakan dalam 2 babak, yaitu babak penyisihan design dan babak final.
+    <br/>
+    <div >
+            <h3>Hadiah</h3>
+            
+                <p>Juara 1 <br/> Rp 2000.000,00</p>
+                <p>Juara 2 <br/> Rp 1500.000,00</p>
+                <p>Juara 3 <br/> Rp 1000.000,00</p>
+            
+    </div>    
 @endsection
 
 @section('rulebook_btn')
